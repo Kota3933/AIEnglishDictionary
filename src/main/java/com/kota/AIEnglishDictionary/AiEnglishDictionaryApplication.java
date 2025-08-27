7cdbd20560586227168d9ff9dbd768783129f082
kota <ortega3933@gmail.com>
2025-08-27 21:45:24 +0900

    Initial commit

---
+++ src/main/java/com/kota/AIEnglishDictionary/AiEnglishDictionaryApplication.java
@@ -0,0 +1,13 @@
+package com.kota.AIEnglishDictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiEnglishDictionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiEnglishDictionaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kota/AIEnglishDictionary/AiEnglishDictionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kota.AIEnglishDictionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiEnglishDictionaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
